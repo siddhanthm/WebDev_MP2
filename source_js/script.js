cc69dafa9a635ae5df632c98b18467cccbd536ca
Siddhanth Maheshwari <siddhanthm@outlook.com>
2016-02-25 05:57:32 -0600

    Adding JavaScript

--- source_js/script.js
+++ source_js/script.js
@@ -1,5 +1,21 @@
 // Write any custom javascript functions here
-console.log("hello");
-$("img").on("click", function(){
-	console.log("wee");
+$(document).ready(function(){
+	$(window).on("scroll", function(){
+		if($(window).scrollTop() > $(".front-page").outerHeight()){
+			$("nav").addClass("visible");
+
+		}else{
+			$("nav").removeClass("visible");
+		}
+	});
+
+	$(".fa-angle-double-up").on("click", function(e){
+		e.preventDefault();
+		$('html, body').animate({scrollTop:0}, "slow");
+	});
+
+	$(".front-page a").on("click", function(e){
+		e.preventDefault();
+		$('html, body').animate({scrollTop: $(".front-page").outerHeight() + $("nav").outerHeight() }, "slow");
+	});
 });
