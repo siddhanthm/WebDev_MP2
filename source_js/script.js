e657c8299b7c449e6cf294b72958bd1679c2ef8e
Siddhanth Maheshwari <siddhanthm@outlook.com>
2016-02-27 15:50:15 -0600

    skills done, intro and work left

--- source_js/script.js
+++ source_js/script.js
@@ -1,5 +1,12 @@
 // Write any custom javascript functions here
 $(document).ready(function(){
+
+
+	var boxes = $(".skills_box");
+	boxes.each(function(){
+		var width = $(this).width();
+		$(this).css({"height": width+"px"});
+	});
 	//hide navbar on front page
 	$(window).on("scroll", function(){
 		if($(window).scrollTop() > $(".front-page").outerHeight()){
@@ -18,7 +25,7 @@ $(document).ready(function(){
 	//scroll to intro
 	$(".front-page a").on("click", function(e){
 		e.preventDefault();
-		$('html, body').animate({scrollTop: $(".front-page").outerHeight() + $("nav").outerHeight() }, "slow");
+		$('html, body').animate({scrollTop: $(".front-page").outerHeight() + 1 }, "slow");
 	});
 
 	//smooth scrolling
@@ -50,4 +57,33 @@ $(document).ready(function(){
 		});
 
 	});
+
+
+
+	var $animation_elements = $('.animation-element');
+	var $window = $(window);
+
+	$window.on('scroll resize', check_if_in_view);
+	$window.trigger('scroll');
+
+	function check_if_in_view() {
+  		var window_height = $window.height();
+  		var window_top_position = $window.scrollTop();
+  		var window_bottom_position = (window_top_position + window_height);
+
+  		$.each($animation_elements, function() {
+    		var $element = $(this);
+    		var element_height = $element.outerHeight();
+    		var element_top_position = $element.offset().top;
+    		var element_bottom_position = (element_top_position + element_height);
+
+    	//check to see if this current container is within viewport
+    		if ((element_bottom_position >= window_top_position) &&
+        		(element_top_position <= window_bottom_position)) {
+      				$element.addClass('in-view');
+    			} else {
+      				$element.removeClass('in-view');
+    			}
+  		});
+	}
 });
